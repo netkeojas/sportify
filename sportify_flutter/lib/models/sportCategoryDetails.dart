c1845d7118e6300f142b054749438d69bbcd1d5a
netkeojas <ojasnetke@gmail.com>
2023-10-18 15:28:30 +0530

    Added comments for better visibility

--- sportify_flutter/lib/models/sportCategoryDetails.dart
+++ sportify_flutter/lib/models/sportCategoryDetails.dart
@@ -1,5 +1,12 @@
 import 'package:sportify_client/sportify_client.dart';
 
+/**
+ * This calss is created to used combined datat from backend DB tables
+ * Structure has data from these Tables from backend and classes in client side
+ * 1. SportCategory { id, name, isGroupSport, popularity }
+ * 2. SportVenueHasSportCategory { id, sportVenueId, sportCategoryId}
+ */
+
 class SportCategoryDetails {
   int? sportCategoryBelongsToVenueId;
   SportCategory? sportCategory;
--- sportify_flutter/lib/models/sportVenueDetails.dart
+++ sportify_flutter/lib/models/sportVenueDetails.dart
@@ -1,6 +1,11 @@
 import 'package:sportify_client/sportify_client.dart';
 import 'sportCategoryDetails.dart';
 
+/// This class is created to use the data fetched from DB in combined and structured way
+/// Structure has data from these Tables from backend and classes in client side
+/// 1. SportVenue - {id, name, address, other details....}
+/// 2. SportCategoryDetails - {sportVenueHasSportCategoryId, sportCategory, sportVenueHasSportCategory}
+
 class SportVenueDetail {
   String? name;
   String? address;
